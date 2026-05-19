64f79acb752511e1435df89c8312fb7bd172651d
mayr3au <mayreauanouzer@gmail.com>
2026-05-19 14:51:52 +0200

    feat: integrate Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import { LangProvider } from '../lib/LangContext';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <LangProvider>
       <Component {...pageProps} />
+      <Analytics />
     </LangProvider>
   );
 }
