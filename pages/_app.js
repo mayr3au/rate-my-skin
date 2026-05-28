1ef98ba1aa8f4f375fed9344ed57b298e23c333f
mayr3au <mayreauanouzer@gmail.com>
2026-05-28 13:25:24 +0200

    feat: add Google Analytics scripts to App component

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,23 @@ import '../styles/globals.css';
 import { LangProvider } from '../lib/LangContext';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
     <LangProvider>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-DMB015RX5X"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-DMB015RX5X');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
