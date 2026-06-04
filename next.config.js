2a26648053f5105303d25e3c6fccca95a5e4c210
mayr3au <mayreauanouzer@gmail.com>
2026-06-04 16:47:13 +0200

    Configure images.domains in next.config.js to support Amazon variations and Unsplash

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'm.media-amazon.com',
+      'media-amazon.com',
+      'images-na.ssl-images-amazon.com',
+      'images.unsplash.com',
+      'ratemyskin.co'
+    ],
+  },
 };
 
 module.exports = nextConfig;
